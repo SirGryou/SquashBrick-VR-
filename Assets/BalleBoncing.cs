7b5913b67f3e0a0a024f0828ab9a54ab706fe906
Merclez <faustingaby34140@gmail.com>
2019-05-29 12:06:53 +0200

    commit FaustinSceneBase

---
+++ Assets/BalleBoncing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalleBoncing : MonoBehaviour
+{
+    [SerializeField]
+    
+    private Vector3 initialVelocity;
+
+    [SerializeField]
+    private float minVelocity = 10f;
+
+    private Vector3 lastFrameVelocity;
+    private Rigidbody rb;
+
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = initialVelocity;
+    }
+
+    private void Update()
+    {
+        lastFrameVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Bounce(collision.contacts[0].normal);
+    }
+
+    private void Bounce(Vector3 collisionNormal)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+
+        Debug.Log("Out Direction: " + direction);
+        rb.velocity = direction * Mathf.Max(speed, minVelocity);
+    }
+}
---
+++ Assets/Scripts/Deplacement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour {
+
+    public int mCurrentIndex = 0;
+    public Vector3[] Transforming;
+    public float speed;
+    public void Yes()
+    {
+
+        mCurrentIndex = Random.Range(1, 6);
+        //var i = mCurrentIndex;
+        Debug.Log("displacement?");
+        //transform.Translate(Transforming[mCurrentIndex] * Time.deltaTime);
+        Debug.Log("displacement");
+         transform.LookAt(Transforming[mCurrentIndex]);
+        
+
+
+        StartCoroutine(Lancer());
+    }
+
+    public void Start()
+    {
+        Yes();
+        
+    }
+    private void Update()
+    {
+
+        Vector3 currentPos = Transforming[mCurrentIndex];
+        transform.position = Vector3.Lerp(transform.position, currentPos, speed * Time.deltaTime);
+    }
+    IEnumerator Lancer()
+    {
+        yield return new WaitForSeconds(5f);
+        Yes();
+    }
+
+
+
+
+
+
+}
